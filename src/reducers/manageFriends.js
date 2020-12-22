8b5aed7355488921e780c3539077a2892a635192
Alexandra Pestruyeva <apestruy@gmail.com>
2020-12-22 20:48:50 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return (
+        {...state, friends: [...state.friends, action.friend]}
+      )
+
+      break;
+    default:
+    return state
+
+  }
 }
+
+let state = {friends: []}
